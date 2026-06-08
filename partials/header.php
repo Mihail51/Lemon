58c7c481b883e169f773d88239fd80ba0b94ea4f
Mihail51 <mihail51@gmail.com>
2026-06-08 11:42:54 +0300

    Add admin link to header

--- partials/header.php
+++ partials/header.php
@@ -55,7 +55,7 @@
 			<?php if (!empty($_SESSION['user_email'])): ?>
 				<a class="auth__link" href="/my-recipes.php">Мои рецепты</a>
 				<a class="auth__link" href="/submit/recipe.php">Добавить рецепт</a>
-				<a class="auth__link" href="/admin/pending.php">Админка</a>
+				<a class="auth__link" href="/admin/recipes.php">Админка</a>
 				<a class="auth__link" href="/logout.php">Выйти</a>
 			<?php else: ?>
 				<a class="auth__link" href="/login.php">Войти</a>
